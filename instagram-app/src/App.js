9052d7d8f1f6d5bc043dd4bb25618ca13b49ec6a
Vaeliwen <zaman@imbriaco.com>
2019-06-03 15:40:48 -0400

    Added searchbar

---
+++ instagram-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Searchbar from './components/Searchbar';
+import dummyData from './dummy-data'
+
+function App() {
+  return (
+    <div>
+      <Searchbar />
+      </div>
+  )
+}
+
+export default App
---
+++ instagram-app/src/components/Searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Searchbar(props) {
+    return (
+        <div className="topbar">
+
+            <input placeholder='Search' />
+        </div>
+    )
+}
+
+export default Searchbar;
